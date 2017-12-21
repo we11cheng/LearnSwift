4f2619a62e8af6be402b73a15513e47e81e6a379
guanweicheng <1396539283@qq.com>
2017-12-21 15:43:24 +0800

    轮播图

--- Gwc_swift/ReportViewController.swift
+++ Gwc_swift/ReportViewController.swift
@@ -7,13 +7,44 @@
 //
 
 import UIKit
+import LLCycleScrollView
 
 class ReportViewController: UIViewController {
-
+    
+    var homeBanner: LLCycleScrollView!
+    
+    func setupBanner() -> Void {
+        self.homeBanner = LLCycleScrollView.init(frame: CGRect.init(x: 0, y: 64, width: kScreenWidth, height: 240));
+        // 网络图，本地图混合
+        let imagesURLStrings = [
+            "http://www.g-photography.net/file_picture/3/3587/4.jpg",
+            "http://img2.zjolcdn.com/pic/0/13/66/56/13665652_914292.jpg",
+            "http://c.hiphotos.baidu.com/image/w%3D400/sign=c2318ff84334970a4773112fa5c8d1c0/b7fd5266d0160924c1fae5ccd60735fae7cd340d.jpg",
+            "http://img3.redocn.com/tupian/20150806/weimeisheyingtupian_4779232.jpg",
+            ];
+//        let titles = ["感谢您的支持1",
+//                      "感谢您的支持2",
+//                      "如果代码在使用过程中出现问题",
+//                      "您可以发邮件到coderjianfeng@foxmail.com您可以发邮件到coderjianfeng@foxmail.com"
+//        ];
+        self.homeBanner.imagePaths = imagesURLStrings
+        //self.homeBanner.titles = titles
+        
+        // 新增图片显示控制
+        self.homeBanner.imageViewContentMode = .scaleToFill
+        self.homeBanner.customPageControlStyle = .image
+        self.homeBanner.pageControlPosition = .center
+        //self.homeBanner.pageControlActiveImage = UIImage.init(named: "lldotActive")
+        //self.homeBanner.pageControlInActiveImage = UIImage.init(named: "lldotInActive")
+        self.homeBanner.customPageControlStyle = .system
+        self.view.addSubview(self.homeBanner)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationItem.title = "报表";
+        self.setupBanner()
     }
 
     override func didReceiveMemoryWarning() {
--- Podfile
+++ Podfile
@@ -5,6 +5,7 @@ target 'Gwc_swift' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
   pod 'Kingfisher', '~> 4.0'
+  pod 'LLCycleScrollView'
 
   # Pods for Gwc_swift
 
